a2598035d0112b6d6284f75823d3813d2d05ff0c
Bruno Toshiaki <brunotoshiaki@hotmail.com>
2022-12-12 08:50:04 -0300

    Criando o projeto

---
+++ src/test/java/br/com/curso/udemy/CursoUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.curso.udemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoUdemyApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
